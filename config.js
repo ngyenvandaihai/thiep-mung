2e5c24109bc1a7093de144b06bf7a059956f2aa4
ngyenvandaihai <88896105+ngyenvandaihai@users.noreply.github.com>
2021-11-30 09:19:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
 const nameGirl = 'Em'; 
-const eventName = 'Chúc mừng  ';
+const eventName = 'Chúc mừng ';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Chúc em tháng 12 tháng cuối cùng năm của tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
-// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
 const giftImage = 'hot-girl.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
