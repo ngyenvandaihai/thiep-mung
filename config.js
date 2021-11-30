b396d511b31626f5eb43dab03539e043732bf29e
ngyenvandaihai <88896105+ngyenvandaihai@users.noreply.github.com>
2021-11-30 09:19:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const nameGirl = 'Em'; 
+const nameGirl = 'Em bé'; 
 const eventName = 'Chúc mừng ';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Chúc em tháng 12 tháng cuối cùng năm của tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
